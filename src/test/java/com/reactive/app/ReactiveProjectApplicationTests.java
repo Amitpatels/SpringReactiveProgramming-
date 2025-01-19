842b6fff1026cd2d83157c6ae219bb63c3c02fe2
amitpatel <patelamit812@gmai.com>
2025-01-19 17:14:57 +0530

    combined with zip mono

--- src/test/java/com/reactive/app/ReactiveProjectApplicationTests.java
+++ src/test/java/com/reactive/app/ReactiveProjectApplicationTests.java
@@ -3,6 +3,8 @@ package com.reactive.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuple3;
 
 @SpringBootTest
 class ReactiveProjectApplicationTests {
@@ -18,13 +20,39 @@ class ReactiveProjectApplicationTests {
 		Mono<String> m1 = Mono.just("Amit work in google")
 				.log();
 
+		//Error mono
+		Mono<String> errorMono = Mono.error(new RuntimeException("Error !!"));
+
 		/*
 		m1.subscribe(data -> {
 			System.out.println("data is ---> : "+ data);
 		});
 		 */
 
+		//consume mono by subscribing
 		m1.subscribe(System.out::println);
+	}
+
+	@Test
+	public void combineMono(){
+		Mono<String> m1 = Mono.just("Amit is working in google");
+		Mono<String> m2 = Mono.just("Salary package is 1.2 crore");
+		Mono<String> m3 = Mono.just("when Amit work in india if Amit working USA salary 5 crore ");
+
+		Mono<Tuple2<String,String>> combinedMono =  Mono.zip(m1,m2);
+
+		Mono<Tuple3<String,String,String>> combinedMono3= Mono.zip(m1,m2,m3);
+
+		combinedMono.subscribe(data -> {
+			System.out.println(data.getT1());
+			System.out.println(data.getT2());
+		});
+
+		combinedMono3.subscribe(data -> {
+			System.out.println(data.getT1());
+			System.out.println(data.getT2());
+			System.out.println(data.getT3());
+		});
 	}
 
 }
